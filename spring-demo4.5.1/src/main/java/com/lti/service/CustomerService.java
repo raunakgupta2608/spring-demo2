4a575b8e1b001a05bede868bcef2b18c1d50b173
raunakgupta2608 <raunakgupta2608@gmail.com>
2019-10-22 05:36:46 -0700

    tenth-commit
    Registerib worked fine for true account number.
    has a lot of sout in every function

---
+++ spring-demo4.5.1/src/main/java/com/lti/service/CustomerService.java
@@ -0,0 +1,28 @@
+package com.lti.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lti.entity.Admin;
+import com.lti.dao.CustomerDao;
+import com.lti.entity.Customer;
+
+@Service
+//business logic
+public class CustomerService 
+{
+
+	
+			@Autowired
+			private CustomerDao customerDao;
+			
+			
+			public void openSavingAccount(Customer customer, Admin admin) {
+				customerDao.openSavingAccount(customer,admin); //ask the dao to save it in the db
+				//sending emailcode here
+			}
+			
+			public Customer get(String email)	{
+				return customerDao.fetch(email);				
+				}
+}
---
+++ spring-demo4.5.1/src/main/java/com/lti/service/RegisterService.java
@@ -0,0 +1,92 @@
+package com.lti.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.lti.dao.RegisterDao;
+import com.lti.entity.Registerib;
+
+@Service
+public class RegisterService 
+{
+	@Autowired
+	private RegisterDao registerDao; 
+	
+	int accnofromfirstrequest;
+	String loginpassfromsecondrequest;
+	String transpassfromsecondrequest;
+	public void fetchAccountNumberFromRegisterIBPage(int accno)
+	{
+		System.out.println("entering registerService controller fetchAccountNumberFromRegisterIBPage");
+		accnofromfirstrequest = accno;
+		System.out.println( accnofromfirstrequest );
+		System.out.println("leaving register controller fetchAccountNumberFromRegisterIBPage");
+	}
+	/*public String checkAccountNumber(Registerib registerib) 
+	{ 
+		System.out.println("enterred register service checkAccountNumber");
+		int flag = registerDao.checkAccountNumber(registerib); 
+		String ret="";
+		if(flag == 1)
+			ret = "setpassword.jsp";
+		else if(flag == -1)
+			ret = "error.jsp";
+		System.out.println("leaving register service checkAccountNumber");
+		
+		return ret;
+	}*/
+	
+	
+	public void fetchPasswordFromSetPasswordPage( Registerib secondreqobj, String loginpass , String transpass )
+	{
+		System.out.println("entering register service fetchPasswordFromSetPasswordPage"); 
+		
+		loginpassfromsecondrequest = loginpass;
+		System.out.println( loginpass );
+		transpassfromsecondrequest = transpass;
+		System.out.println( transpass );
+		saveDataFromFirstAndSecondRequest( secondreqobj );
+		
+		System.out.println("leaving register service fetchPasswordFromSetPasswordPage");
+	}
+	
+	public String saveDataFromFirstAndSecondRequest( Registerib secondreqobj )
+	{
+		System.out.println("entering register service saveDataFromFirstAndSecondRequest"); 
+		
+		String ret="";
+		
+		secondreqobj.setAccno(accnofromfirstrequest);
+		secondreqobj.setLoginpass(loginpassfromsecondrequest);
+		secondreqobj.setTranspass(transpassfromsecondrequest);
+		
+		int statuscode = registerDao.checkAccountNumber( accnofromfirstrequest );
+		if ( statuscode == 1 )
+		{
+			registerDao.saveDataFromFirstAndSecondRequest( secondreqobj );
+		}
+		else if ( statuscode == -1 )
+		{
+			ret = "error.jsp";
+		}
+		System.out.println("leaving register service saveDataFromFirstAndSecondRequest");
+		return ret;
+	}
+	
+	
+	
+	
+	/*
+	 * public String checkAccountNumber(Registerib registerib) {
+	 * System.out.println("enterred register service checkAccountNumber"); int flag
+	 * = getAccountNumber(registerib); String ret=""; if(flag == 1) ret =
+	 * "setpassword.jsp"; else if(flag == -1) ret = "error.jsp";
+	 * System.out.println("leaving register service checkAccountNumber");
+	 * 
+	 * return ret; } public int getAccountNumber(Registerib registerib) {
+	 * System.out.println("enterred register service getAccountNumber"); int
+	 * statuscode = registerDao.getAccountNumber(registerib);
+	 * System.out.println("leaving register service getAccountNumber"); return
+	 * statuscode; }
+	 */
+	 
+}
