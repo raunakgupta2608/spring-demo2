37aee51c8cf069c89c03ff14ed0b19b32b3419ff
raunakgupta2608 <raunakgupta2608@gmail.com>
2019-10-22 07:32:47 -0700

    otp entity class

---
+++ Otp.java
@@ -0,0 +1,43 @@
+package com.lti.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="otp")
+public class Otp 
+{
+	@Id
+	@GeneratedValue ( strategy = GenerationType.AUTO, generator ="otpid" )
+/*	@SequenceGenerator ( name ="otpid", initialValue=10, sequenceName="otpid")
+	@GeneratedValue ( strategy = GenerationType.SEQUENCE, generator ="otpid" )*/
+	@Column(name="otpid")
+	private int otpid;
+	@Column(name="accno")
+	private int accno;
+	@Column(name="otp")
+	private int otp;
+	public int getOtpid() {
+		return otpid;
+	}
+	public void setOtpid(int otpid) {
+		this.otpid = otpid;
+	}
+	public int getAccno() {
+		return accno;
+	}
+	public void setAccno(int accno) {
+		this.accno = accno;
+	}
+	public int getOtp() {
+		return otp;
+	}
+	public void setOtp(int otp) {
+		this.otp = otp;
+	}
+	
+}
